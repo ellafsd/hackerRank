2a14c259ed0e6f01ea0d59b32acb96dad4c1ebdc
lsil Avunduk <lsilavunduk@lsils-MacBook-Pro.local>
2025-09-22 16:51:49 +0300

    Add Q09-MiniMaxSum solution

---
+++ Q09-MiniMaxSum.js
@@ -0,0 +1,19 @@
+let arr =[1,3,5,7,9];
+
+function miniMaxSum(arr) {
+    // Write your code here
+    let sumMin = 0;
+    let sumMax = 0;
+    
+    for(let i=0; i<4; i++){
+        sumMin += arr[i];
+    }
+
+      for(let i=1; i<5; i++){
+        sumMax += arr[i];
+    }
+    
+    console.log(sumMin, sumMax);
+} 
+
+miniMaxSum(arr);
